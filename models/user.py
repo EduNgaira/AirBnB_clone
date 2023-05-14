3a10c139ba2ad042dbaaaae057e19ebe6626390c
Edu Ngaira <ngairaedwin@gmail.com>
2023-05-14 07:26:50 +0000

    Executable py files

---
+++ models/user.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+User class
+"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """User.
+
+    Attributes:
+        email (str): User email.
+        password (str): User password.
+        first_name (str): User first name.
+        last_name (str): User last name.
+    """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
